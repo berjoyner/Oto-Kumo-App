b943170785357d56402503c217ae9e273c40d8ee
Berjoyner <joyner.amber@gmail.com>
2014-05-12 15:35:42 -0400

    Shizz Working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,48 +1,46 @@
+/////Pulling Client Authorized ID from SoundCloud
+
 SC.initialize({
   client_id: '38260eb937ab3e39bd79b1344c8fee9c'
 });
 
 $(document).ready(function() {
 
-$('form').on('submit', function(e){
+/////Pulling userTrack input from form
+
+$('form').on('submit', function(e){ 
 	e.preventDefault();
 
 	var userTrack = $('#find').val();
 	console.log(userTrack);
 
+///// SoundCloud's own SDK for Javascript- GET from database 
+
 	SC.get('/tracks', { genres: userTrack, bpm: { from: 120 } }, function(tracks) {
 	  console.log(tracks);
 	  console.log(loadTracks);
 		
-	  var listing = tracks.data;
-	  console.log(listing);
+	  var listing = tracks;
+	  var html ='';
+	  
+	  for(var i=0, l=listing.length; i<l; i++) {
+        var obj = listing[i];
 
-	  SC.oEmbed(tracks.permalink_url,
-    		document.getElementById('player')
+	  console.log(obj);
 
-		);
+	  var genres = obj.genres;
+
+///// SoundCloud's own SDK for Javascript- Embedding the Video Player
+
+	  SC.oEmbed(obj.permalink_url,
+    		document.getElementById('player'));
+
+	  	html += '<ul class="songList" </ul>';
+
+		} //end for loop
 	});
 });
+
 });
-
-	  // var tracker = {
-	  // 		url: $('permalink_url').val(),
-	  // };
-
-	  // console.log(tracker);
-// 	  var html = '';
-
-// 	  for(var i=0, l=listing.length; i<l; i++) {
-//         var obj = listing[i].data;
-
-// 	  var genre = obj.genre;
-
-// 	  alert(obj.genre);
-
-// 	  // htm += '<ul '+genre+' class="songList" </ul>';
-
-// SC.get('/tracks/293', function(track){
-//     SC.oEmbed(track.permalink_url,
-//     document.getElementById('player'));
-//    });
+	
 
